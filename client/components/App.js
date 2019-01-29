33a0081a919318ac68084b5133e36596e039d40e
XReid <xmeister86@gmail.com>
2019-01-28 22:24:27 -0800

    updated scripts

--- client/components/App.js
+++ client/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1>I'm a sample Component!</h1>
             </div>
         );
     }
